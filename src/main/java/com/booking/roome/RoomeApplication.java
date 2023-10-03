b9c2866ef89e15e7938c13a2ccc32c1ff1330559
Yousof Kortam <yousofkortam@outlook.com>
2023-10-03 22:10:56 +0300

    intial commit

---
+++ src/main/java/com/booking/roome/RoomeApplication.java
@@ -0,0 +1,13 @@
+package com.booking.roome;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RoomeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RoomeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/booking/roome/RoomeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.booking.roome;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RoomeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
